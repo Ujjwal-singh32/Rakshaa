6d9db9e1c546260a4e6af5314e2629841cadae38
Aditya-170 <pramodkumar3615@gamil.com>
2025-04-10 08:36:59 +0530

    Add Auth Beckend

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -18,6 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -25,5 +27,6 @@ export default function RootLayout({ children }) {
         {children}
       </body>
     </html>
+    
   );
 }
---
+++ src/models/patientModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const patientSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  address: { type: String },
+  emergencyContact: { type: String },
+  bloodType: { type: String },
+  allergies: { type: String },
+  medications: { type: String },
+  weight: { type: String },
+  height: { type: String },
+  age: { type: String },
+  password: { type: String, required: true },
+  profilePic: { type: String },
+});
+
+const patientModel = mongoose.model("Patient", patientSchema);
+export default patientModel;
